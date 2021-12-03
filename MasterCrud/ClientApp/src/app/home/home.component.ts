72729f469acf9c34210d6e65a0c19ad43373e382
Leonardo Hernández <leonardo.hernandez@genesys.cl>
2021-12-03 00:35:05 -0500

    Inicialización de Front-end

--- MasterCrud/ClientApp/src/app/home/home.component.ts
+++ MasterCrud/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,25 @@
 import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+
+  datos: any[] = [];
+
+  constructor(private http: HttpClient) {
+    http.get('/api/ciudades/').subscribe((data: any) => {
+      this.datos = data;
+      console.log(this.datos);
+    });
+  }
+
+  getCity(id): void {
+    http.get('/api/ciudades/').subscribe((data: any) => {
+      this.datos = data;
+      console.log(this.datos);
+    });
+  }
 }
